a14897b4e7520b2571f08db9d7b4c0e990d3f208
Link1515 <h26281660@gmail.com>
2024-09-03 13:26:36 +0800

    add endpoints

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using dotnet_jwt;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,7 +41,55 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+// 登入取得 jwt
+app.MapPost("/signin", (LoginViewModel login, JwtHelpers jwt) =>
+{
+    if (ValidateUser(login))
+    {
+        var token = jwt.GenerateToken(login.Username);
+        return Results.Ok(new { token });
+    }
+
+    return Results.BadRequest();
+})
+.WithName("SignIn")
+.AllowAnonymous();
+
+// 取得 token 所有的 claims
+app.MapGet("/claims", (ClaimsPrincipal user) =>
+{
+    return Results.Ok(user.Claims.Select(p => new { p.Type, p.Value }));
+})
+.WithName("Claims")
+.RequireAuthorization();
+
+app.MapGet("/username", (ClaimsPrincipal user) =>
+{
+    return Results.Ok(user.Identity?.Name);
+})
+.WithName("Username")
+.RequireAuthorization();
+
+app.MapGet("/isInRole", (ClaimsPrincipal user, string name) =>
+{
+    return Results.Ok(user.IsInRole(name));
+})
+.WithName("IsInRole")
+.RequireAuthorization();
+
+app.MapGet("/jwtid", (ClaimsPrincipal user) =>
+{
+    return Results.Ok(user.Claims.FirstOrDefault(p => p.Type == "jti")?.Value);
+})
+.WithName("JwtId")
+.RequireAuthorization();
+
 app.Run();
+
+bool ValidateUser(LoginViewModel login)
+{
+    return true;
+}
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
